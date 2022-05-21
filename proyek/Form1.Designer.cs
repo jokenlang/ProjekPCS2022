86a475f50580d38b257e5cebdcca31f9dcabb4cb
ZiiiE <jonathankenzie84@gmail.com>
2022-05-21 20:29:50 +0700

    test

--- proyek/Form1.Designer.cs
+++ proyek/Form1.Designer.cs
@@ -31,16 +31,17 @@ namespace proyek
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new ePOSOne.btnProduct.Button_WOC();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(175, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(166, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Selamat Datang di Bioskop GLBB";
             // 
             // label2
             // 
@@ -51,14 +52,31 @@ namespace proyek
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
+            // btnLogin
+            // 
+            this.btnLogin.BorderColor = System.Drawing.Color.Silver;
+            this.btnLogin.ButtonColor = System.Drawing.Color.Red;
+            this.btnLogin.Location = new System.Drawing.Point(105, 330);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.OnHoverBorderColor = System.Drawing.Color.Gray;
+            this.btnLogin.OnHoverButtonColor = System.Drawing.Color.Yellow;
+            this.btnLogin.OnHoverTextColor = System.Drawing.Color.Gray;
+            this.btnLogin.Size = new System.Drawing.Size(169, 40);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextColor = System.Drawing.Color.White;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(395, 450);
+            this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -69,6 +87,7 @@ namespace proyek
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private ePOSOne.btnProduct.Button_WOC btnLogin;
     }
 }
 
