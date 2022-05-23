a1055bed6574295c0d409b42e096080082904281
jlairlangga <john_la@mhs.istts.ac.id>
2022-05-23 10:50:44 +0700

    test

--- proyek/MasterKasir.Designer.cs
+++ proyek/MasterKasir.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button_WOC1 = new ePOSOne.btnProduct.Button_WOC();
             this.SuspendLayout();
+            // 
+            // button_WOC1
+            // 
+            this.button_WOC1.BorderColor = System.Drawing.Color.Silver;
+            this.button_WOC1.ButtonColor = System.Drawing.Color.Red;
+            this.button_WOC1.Location = new System.Drawing.Point(273, 136);
+            this.button_WOC1.Name = "button_WOC1";
+            this.button_WOC1.OnHoverBorderColor = System.Drawing.Color.Gray;
+            this.button_WOC1.OnHoverButtonColor = System.Drawing.Color.Yellow;
+            this.button_WOC1.OnHoverTextColor = System.Drawing.Color.Gray;
+            this.button_WOC1.Size = new System.Drawing.Size(75, 23);
+            this.button_WOC1.TabIndex = 0;
+            this.button_WOC1.Text = "button_WOC1";
+            this.button_WOC1.TextColor = System.Drawing.Color.White;
+            this.button_WOC1.UseVisualStyleBackColor = true;
             // 
             // MasterKasir
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_WOC1);
             this.Name = "MasterKasir";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MasterKasir";
@@ -44,5 +61,7 @@
         }
 
         #endregion
+
+        private ePOSOne.btnProduct.Button_WOC button_WOC1;
     }
 }
