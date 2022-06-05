85555742ec12e815167202d51a776ad8d7b26caf
ZiiiE <jonathankenzie84@gmail.com>
2022-06-06 00:08:12 +0700

    update tampilan masterCS tipis"

--- proyek/MasterCS.Designer.cs
+++ proyek/MasterCS.Designer.cs
@@ -42,15 +42,18 @@
             // 
             // btnClear
             // 
-            this.btnClear.BorderColor = System.Drawing.Color.Silver;
-            this.btnClear.ButtonColor = System.Drawing.Color.Transparent;
+            this.btnClear.BorderColor = System.Drawing.Color.Transparent;
+            this.btnClear.ButtonColor = System.Drawing.Color.Silver;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(202, 357);
+            this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(191)))), ((int)(((byte)(177)))));
+            this.btnClear.Location = new System.Drawing.Point(152, 290);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClear.Name = "btnClear";
             this.btnClear.OnHoverBorderColor = System.Drawing.Color.Gray;
             this.btnClear.OnHoverButtonColor = System.Drawing.Color.Yellow;
             this.btnClear.OnHoverTextColor = System.Drawing.Color.Gray;
-            this.btnClear.Size = new System.Drawing.Size(146, 41);
+            this.btnClear.Size = new System.Drawing.Size(110, 33);
             this.btnClear.TabIndex = 49;
             this.btnClear.Text = "Clear";
             this.btnClear.TextColor = System.Drawing.Color.White;
@@ -59,15 +62,18 @@
             // 
             // btnUpdate
             // 
-            this.btnUpdate.BorderColor = System.Drawing.Color.Silver;
+            this.btnUpdate.BorderColor = System.Drawing.Color.Transparent;
             this.btnUpdate.ButtonColor = System.Drawing.Color.Lime;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdate.Location = new System.Drawing.Point(29, 357);
+            this.btnUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(191)))), ((int)(((byte)(177)))));
+            this.btnUpdate.Location = new System.Drawing.Point(22, 290);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.OnHoverBorderColor = System.Drawing.Color.Gray;
             this.btnUpdate.OnHoverButtonColor = System.Drawing.Color.Yellow;
             this.btnUpdate.OnHoverTextColor = System.Drawing.Color.Gray;
-            this.btnUpdate.Size = new System.Drawing.Size(146, 41);
+            this.btnUpdate.Size = new System.Drawing.Size(110, 33);
             this.btnUpdate.TabIndex = 47;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.TextColor = System.Drawing.Color.White;
@@ -78,10 +84,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.label1.Location = new System.Drawing.Point(292, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(219, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 36);
+            this.label1.Size = new System.Drawing.Size(196, 29);
             this.label1.TabIndex = 35;
             this.label1.Text = "Cleaning Service";
             // 
@@ -93,26 +98,30 @@
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 48);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 39);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(798, 237);
+            this.dataGridView1.Size = new System.Drawing.Size(598, 193);
             this.dataGridView1.TabIndex = 34;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // btnExit
             // 
-            this.btnExit.BorderColor = System.Drawing.Color.Silver;
+            this.btnExit.BorderColor = System.Drawing.Color.Transparent;
             this.btnExit.ButtonColor = System.Drawing.Color.Red;
-            this.btnExit.Location = new System.Drawing.Point(723, 358);
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(191)))), ((int)(((byte)(177)))));
+            this.btnExit.Location = new System.Drawing.Point(542, 291);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
             this.btnExit.OnHoverBorderColor = System.Drawing.Color.Gray;
             this.btnExit.OnHoverButtonColor = System.Drawing.Color.Yellow;
             this.btnExit.OnHoverTextColor = System.Drawing.Color.Gray;
-            this.btnExit.Size = new System.Drawing.Size(75, 41);
+            this.btnExit.Size = new System.Drawing.Size(56, 33);
             this.btnExit.TabIndex = 33;
             this.btnExit.Text = "Exit";
             this.btnExit.TextColor = System.Drawing.Color.White;
@@ -122,10 +131,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(233, 312);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton2.Location = new System.Drawing.Point(175, 254);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(69, 21);
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
             this.radioButton2.TabIndex = 52;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Rusak";
@@ -134,10 +143,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(151, 312);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton1.Location = new System.Drawing.Point(113, 254);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(69, 21);
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
             this.radioButton1.TabIndex = 51;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Bagus";
@@ -146,26 +155,28 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(25, 312);
+            this.label4.Location = new System.Drawing.Point(19, 254);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.Size = new System.Drawing.Size(43, 13);
             this.label4.TabIndex = 50;
             this.label4.Text = "Status :";
             // 
             // id
             // 
-            this.id.Location = new System.Drawing.Point(711, 9);
+            this.id.Location = new System.Drawing.Point(533, 7);
+            this.id.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.id.Name = "id";
-            this.id.Size = new System.Drawing.Size(105, 22);
+            this.id.Size = new System.Drawing.Size(80, 20);
             this.id.TabIndex = 53;
             this.id.Visible = false;
             // 
             // MasterCS
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(191)))), ((int)(((byte)(177)))));
-            this.ClientSize = new System.Drawing.Size(828, 414);
+            this.ClientSize = new System.Drawing.Size(621, 336);
             this.Controls.Add(this.id);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
@@ -175,6 +186,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnExit);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MasterCS";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MasterCS";
